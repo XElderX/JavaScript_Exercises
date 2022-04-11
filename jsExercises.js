4b316de19d1bd6a9e9d010d5aafea8918f8a88ae
unknown <dalius.badley@gmail.com>
2022-04-11 21:54:04 +0300

    Added finished JS excercise

---
+++ jsExercises.js
@@ -0,0 +1,10 @@
+ // Užduotis:
+// sukurkite 2 kintamuosius pavadinimais “myName” ir “myLastname”
+// jie turi talpinti savyje jūsų vardą ir pavardę
+// naudodamies šiaisi kintamaisiais į konsolę išspausdinkite, pasisveikinimą: “Hi Jonas Jonaitis”.
+
+
+/* let myName="Dalius";
+let myLastname="Kriauciunas";
+
+console.log(`Hi ${myName}  ${myLastname}`) */
