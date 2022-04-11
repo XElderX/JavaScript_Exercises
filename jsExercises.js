d82273272653107466bb7102a15f175cdb2e6b95
unknown <dalius.badley@gmail.com>
2022-04-11 22:12:15 +0300

    2nd execise Done

--- jsExercises.js
+++ jsExercises.js
@@ -8,3 +8,19 @@
 let myLastname="Kriauciunas";
 
 console.log(`Hi ${myName}  ${myLastname}`) */
+
+
+// Uždavinukas:
+// Turime stringą “Mano vardas {X}, aš esu profesionalus {atletas/-ė} !”. Sukurkite algoritmą, kuris išluptų vardą ir profesiją iš tokio stringo. Algoritmas, turi veikti su bet kokio ilgio string’ais bei naudoti tik indexOf, lastIndexOf is slice metodus.
+
+/* let string = "Mano vardas {Dalius}, aš esu profesionalus {atletas} !";
+
+let vardasString = string.slice(string.indexOf('{') + 1, string.indexOf('}'));
+let profString = string.slice(string.lastIndexOf('{') + 1, string.lastIndexOf('}'));
+
+console.log(`${vardasString} - ${profString}`) */
+
+
+
+
+
