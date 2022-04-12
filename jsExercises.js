9d9799320a5d56eb706fd3b9da31b957ba68a15f
unknown <dalius.badley@gmail.com>
2022-04-12 14:52:58 +0300

    Update:#5 done

--- jsExercises.js
+++ jsExercises.js
@@ -46,9 +46,80 @@ else {
 } */
 
 
-// Exercise: sukurkite vartotojo prieėjimo prie svetainės vedlį (naudojant prompt() ir alert()).
+// #4 Exercise: sukurkite vartotojo prieėjimo prie svetainės vedlį (naudojant prompt() ir alert()).
 //  Vedlys vieną po kito užduos klausimus: koks jūsų vardas, kiek jums metų. Jei vartotojas įveda 
 //  netaisyklingą inputą jam reiktų pranešti, jog praėjęs įvedimas buvo neteisingas prieš klausiant
 //   dar kartą (daryti su alert() bei prompt() f-jom). Vedlys turi pasveikinti vartotoją jeigu jam 
 //   leidžiama prisijungti arba paapgailestauti jei ne.
 
+/* let vardas = prompt("Koks jusu vardas?");
+typeof vardas !=='string' ? alert("tai ne vardas")
+: vardas.length <3 ? alert("jusu vardas per trumpas")
+: true, console.log(`Sveiki, ${vardas}`) 
+let amzius = prompt("Kiek jums metu?");
+if (amzius <=0){
+    alert("jus galbut dar net negimes?")
+}
+else if(amzius<18){
+    alert("sorry, bet tu neturi 18")
+}
+else{
+    true, console.log("Jus esate suauges, todel ok")
+
+}
+alert(`Sveiki atvyke ${vardas}, kadangi jums ${amzius} metu, jus esate laukiamas pas mus`)
+
+ */
+
+// #5 Užduotis: 
+
+// 1)patikinkite ar mūsų programos, kurios naudojasi atsitiktiniais skaičiais yra “fair”.
+//  “A Fair coin” yra apibrėžiama kaip moneta, kurios metimų skaičiui artėjant prie begalybės,
+// atvejų kai iškrenta “H” ir “S” artėja prie 50/50. Sukaupkime rezultatus masyve ir suskaičiuokime,
+// koks buvo vieno ir kito rezultato santykis (prisiminkite alternatyvą su counteriais, bet nenaudokite).
+
+/* 
+let coin;
+let countFair = null;
+let countUnFair = null;
+const coinArray= []
+
+for(i=0;i<100;i++){
+    coin=Math.floor(Math.random() * 2);
+    coin<1 ? (coin = "fair coin", countFair++) : (coin = "unfair coin", countUnFair++)
+    console.log(`${i+1} bandymu iskrito: ${coin}`)
+    coinArray.push(coin);
+}
+let ratio =  (countFair > countUnFair) ? "Fair coin iskrito " + countFair/countUnFair +" karto dazniau" 
+:(countFair < countUnFair) ? "UnFair coin iskrito " +countUnFair/countFair +" karto dazniau" 
+: 1/1
+
+///patikrinimas kiek kartu iskrito
+console.log(`Fair coin iskrito ${countFair} kartu`)
+console.log(`Unfair coin iskrito ${countUnFair} kartu`)
+console.log(coinArray) // Visos reiksmes masyve
+console.log(`Santykis: ${ratio}`) */
+
+// 2)Turime masyvą vardų - atspausdinti vardus atbuline tvarka (ne kiekvienas vardas turi būti atbulai,
+//  bet visa aibė A, B, C → C, B, A). Pasunkinimas: turite nesukurti naujo masyvo.
+/* let varduMasyvas = ["Jonas", "Klaudas", "Frenkis", "Dziuze", "Kleopatra", "Pranas"];
+
+for(i=varduMasyvas.length-1; i>=0; i--){
+    console.log(varduMasyvas[i]);
+} */
+
+
+// 3)Turime masyvą vardų - parašykite algoritmą, kuris pasakytų ar yra konkretus vardas tame masyve. 
+// Turite nesukurti naujo masyvo. Turite atspausdinti “Taip”/”Ne”. (search)
+
+/* let varduMasyvas = ["Jonas", "Klaudas", "Frenkis", "Dziuze", "Kleopatra", "Pranas"];
+(varduMasyvas.includes('Kleopatra') === true) ? console.log("taip") : console.log("Ne") */
+
+
+
+// 4)Turime masyvą vardų - iš to masyvo sukonstruokite kitą masyvą, kuriame yra vardai, ilgesni nei 6 
+// simbiliai. Programoje turi būti max 2 masyvai. Gale programos naują masyvą atspausdinkite. (filter)
+
+const varduMasyvas = ["Jonas", "Klaudas", "Frenkis", "Dziuze", "Kleopatra", "Pranas"];
+const newVarduMasyvas = varduMasyvas.filter(sixLetter => sixLetter.length > 6);
+console.log(newVarduMasyvas)
